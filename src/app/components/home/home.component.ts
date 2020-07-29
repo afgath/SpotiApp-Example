418ca4fc994c96575a17c5652f2b80777e3ad033
Afgath <andresf_giron@coomeva.com.co>
2020-07-29 16:21:25 -0500

    Se sube actualización de paquetes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  // paises : any[] = [];
-  constructor( ) { 
-    // this.http.get('https://restcountries.eu/rest/v2/lang/es')
-    // .subscribe( (response: any) => {
-    //   this.paises = response;
-    //   // console.log(paises);
-    // } );
+  nuevasCanciones: any[] = [];
+  loading: boolean;
+
+  error: boolean;
+  mensajeError: string;
+
+  constructor( private spotify: SpotifyService ) {
+
+    this.loading = true;
+    this.error = false;
+
+    this.spotify.getNewReleases()
+        .subscribe( (data: any) => {
+          this.nuevasCanciones = data;
+          this.loading = false;
+        }, ( errorServicio ) => {
+
+          this.loading = false;
+          this.error = true;
+          console.log(errorServicio);
+          this.mensajeError = errorServicio.error.error.message;
+
+        });
+
   }
 
-  ngOnInit() {
-  }
+
 
 }
